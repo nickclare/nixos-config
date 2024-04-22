4639e44c55ec31de63d54e49b81afcec02279e1a
Nicholas Clare <nick@conjugate.dev>
2024-04-22 11:56:46 +0800

    wip - home-manager

--- flake.nix
+++ flake.nix
@@ -9,15 +9,28 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
   };
 
-  outputs = { self, nixpkgs, sops-nix, ... }@inputs: {
+  outputs = { self, nixpkgs, sops-nix, home-manager, ... }@inputs: {
     
     nixosConfigurations.nixdev = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = {inherit inputs;};
       modules = [
         ./configuration.nix
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          home-manager.users.developer = ./home-manager/home.nix;
+        }
       ];
     };
   };
---
+++ home-manager/home.nix
@@ -0,0 +1,58 @@
+{config, pkgs, ...}:
+{
+  home.username = "developer";
+  home.homeDirectory = "/home/developer";
+
+  home.packages = with pkgs; [
+    rustup
+
+    # dev tools
+    ripgrep
+    jq
+    yq-go
+    eza
+    fzf
+
+    # networking tools
+    dnsutils
+    nmap
+    ipcalc
+   
+    # misc
+    cowsay
+    file
+    which
+    tree
+    gnused
+    gnutar
+    gawk
+    zstd
+    gnupg
+  ];
+
+  programs.git = {
+      enable = true;
+      userName = "Nicholas Clare";
+      userEmail = "nick@conjugate.dev";
+  };
+      
+  programs.fish = {
+    enable = true;
+    shellAbbrs = {
+      ls = "eza";
+    };
+  };
+
+  # This value determines the home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "23.11";
+
+  # Let home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
