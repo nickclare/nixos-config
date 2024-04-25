5e53253ab36b80e291475519e498b2b7456a8d79
Nicholas Clare <nick@conjugate.dev>
2024-04-25 10:47:04 +0800

    wip - home-manager

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS configuration flake for network services";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
       
     sops-nix = {
       url = "github:Mic92/sops-nix";
@@ -10,7 +10,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -21,22 +21,24 @@
 
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: let inherit (self) outputs; in {
+  outputs = { self, nixpkgs, sops-nix, home-manager, ... }@inputs: let inherit (self) outputs; in {
     
     nixosConfigurations.nixdev = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = {inherit inputs;};
       modules = [
         ./configuration.nix
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          home-manager.users.developer = ./home-manager/home.nix;
+          home-manager.extraSpecialArgs = {inherit inputs outputs;};
+        }
       ];
     };
   };
-
-  homeConfigurations."developer@nixdev" = home-manager.lib.homeManagerConfiguration {
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    extraSpecialArgs = {inherit inputs outputs;};
-
-    modules = [./home-manager/home.nix];
-  };
-
 }
+
